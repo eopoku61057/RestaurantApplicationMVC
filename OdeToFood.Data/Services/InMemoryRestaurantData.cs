7056421b5e41038eec8de1c0f2cd5671252068a1
eopoku61057 <opokuemmanuel2015@gmail.com>
2022-05-12 08:01:33 -0500

    Initial commit for mvc project

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,57 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pizza hut", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French },
+                new Restaurant { Id = 3, Name = "Mano Groove", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(restaurants => restaurants.Id) + 1;
+        }
+
+        public void Delete(int id)
+        {
+            var restuarant = Get(id);
+            if (restuarant != null)
+            {
+                restaurants.Remove(restuarant);
+            }
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(restaurants => restaurants.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var existing = Get(restaurant.Id);
+            if (existing != null)
+            {
+                existing.Name = restaurant.Name;
+                existing.Cuisine = restaurant.Cuisine;
+            }
+        }
+    }
+}
---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,50 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = db.Restaurants.Find(id);
+            db.Restaurants.Remove(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            var list = db.Restaurants.OrderBy(r => r.Name).Select(x => x);
+            return list;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            
+        }
+    }
+}
